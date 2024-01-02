7024efebc1456fb4c38a54a4c96464e84cf802f7
unknown <nabilsaputra25@gmail.com>
2024-01-02 18:30:10 +0800

    commit message

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(250)->create();
+
+        User::create([
+            'name' => 'Muhammad Nabil Saputra',
+            'email' => 'c030322155@mahasiswa.poliban.ac.id',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'roles' => 'mahasiswa',
+        ]);
+    }
+}
